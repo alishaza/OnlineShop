773994f143b7356a3036c81f3dba974719c19dd1
alishazaei <alishazaeei@gmail.com>
2023-01-12 19:45:04 +0330

    Nav Bar Entity

---
+++ src/main/java/com/Shopping/onlineshop/entities/site/Nav.java
@@ -0,0 +1,46 @@
+package com.Shopping.onlineshop.entities.site;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Nav {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String title;
+    private String link;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
---
+++ src/test/java/com/Shopping/onlineshop/OnlineshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Shopping.onlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
