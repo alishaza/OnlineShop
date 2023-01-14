87c34c3e7840dc963c3c7740d8d223c1e80484ce
alishazaei <alishazaeei@gmail.com>
2023-01-14 21:59:29 +0330

    Site Service

--- src/main/java/com/Shopping/onlineshop/Services/site/BlogService.java
+++ src/main/java/com/Shopping/onlineshop/Services/site/BlogService.java
@@ -1,7 +1,11 @@
 package com.Shopping.onlineshop.Services.site;
 
 import com.Shopping.onlineshop.Repositories.site.BlogRepository;
+import com.Shopping.onlineshop.Repositories.site.BlogRepository;
 import com.Shopping.onlineshop.entities.site.Blog;
+import com.Shopping.onlineshop.entities.site.Blog;
+import com.Shopping.onlineshop.entities.site.Nav;
+import com.Shopping.onlineshop.exceptions.DataNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,24 +14,47 @@ import java.util.List;
 @Service
 public class BlogService {
     @Autowired
-    private BlogRepository blogRepository;
+    private BlogRepository BlogRepository;
+    public Blog getById(long id ){
+        var data = BlogRepository.findById(id);
+        if (data.isPresent()) return data.get();
+        return null;
+    }
+    public Blog search(String keyword){
+        var data = BlogRepository.findAllByEnableIsTrueAndTitleContainsOrDescriptionNotContains(keyword);
+        if (data != null) return data;
+        return null;
+    }
+    public Blog addBlog(Blog Blog){
+        return BlogRepository.save(Blog);
+    }
 
-    public List<Blog> showBlogs()
+    public Blog updateُBlog(Blog Blog) throws DataNotFoundException {
+        var oldBlog = getById(Blog.getId());
+
+        if ( Blog == null)
+            throw new DataNotFoundException("data with id "+Blog.getId()+" is not found ");
+
+        oldBlog.setImage(oldBlog.getImage());
+        oldBlog.setTitle(oldBlog.getTitle());
+        oldBlog.setEnable(oldBlog.isEnable());
+        oldBlog.setDescription(oldBlog.getDescription());
+        oldBlog.setSubtitle(oldBlog.getSubtitle());
+        return BlogRepository.save(oldBlog);
+    }
+
+    public void increaseVisitCount(Blog blog)
     {
-       return  blogRepository.findAllByEnableIsTrue();
+        blog.setVisitcount(blog.getVisitcount());
+        BlogRepository.save(blog);
     }
-
-
-    public List<Blog> searchBlogs(String text)
-    {
-        return  blogRepository.findAllByEnableIsTrueAndTitleContainsOrDescriptionNotContains(text);
+    public boolean deleteById(long id) throws DataNotFoundException {
+        var oldData = getById(id);
+        if (oldData == null)
+            throw  new DataNotFoundException("data with id "+id+" is not found ");
+        BlogRepository.deleteById(id);
+        return true;
     }
-
-    public Blog addBlog(Blog blog){
-       return blogRepository.save(blog);
-    }
-
-
 
 
 
